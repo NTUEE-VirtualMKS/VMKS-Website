1d5a0a1c876934dcc5afe7f55e4bbbdbed450c9d
謝沅瓏 <hsiehyuanlung@xieyuanlongde-MacBook-Pro.local>
2023-07-29 13:47:37 +0800

    EDITOR-#12 update RTEditor

--- frontend/src/component/Manager/RTEditor.tsx
+++ frontend/src/component/Manager/RTEditor.tsx
@@ -1,12 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-
-function RTEditor () {
+import 'react-quill/dist/quill.snow.css'  //customize settings
+function RTEditor() {
   const [value, setValue] = useState('')
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['link', 'image'],
+    ['blockquote', 'code-block'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['clean']                                         // remove formatting button
+  ];
+  const module = {
+    toolbar: toolbarOptions,
+  };
 
-  return(
-    <ReactQuill theme="snow" value={value} onChange={setValue} />
-  );
+  return (<ReactQuill modules={module} theme="snow" value={value} onChange={setValue} />);
 }
 export default RTEditor;
