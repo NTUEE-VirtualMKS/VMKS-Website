98df0aa1691fc466f960b8486775747ca2f330e9
HubertC1 <bb.chang@gmail.com>
2023-07-29 19:35:07 +0800

    EDITOR-#34 only laser-cutter is clickable

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -6,6 +6,11 @@ const MapPage = () => {
     <>
       <div>Map Page</div>
       <p>應該要有2D地圖，並連結3D地圖</p>
+        <img src="/images/interior2D.jpg" alt="Interior2D" useMap="#interior2d"></img>
+        <map name="interior2d">
+        
+          <area shape="rect" coords="962,0,1086,68" alt="Laser" href="/button"></area>
+        </map>
       <button onClick={() => navigate(-1)}>go back</button>
     </>
   )
