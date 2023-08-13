f9cc9344bb0290534388db7e879889e16d94dfc5
謝沅瓏 <hsiehyuanlung@xieyuanlongde-MacBook-Pro.local>
2023-08-13 14:49:43 +0800

    EDITOR-#102 simple UI for material paper

--- frontend/src/component/MaterialAndTool/Material.tsx
+++ frontend/src/component/MaterialAndTool/Material.tsx
@@ -6,22 +6,22 @@ import { MaterialList } from './MaterialList';
 import useFetchMaterialList from './UseFetchMaterialList'
 import useFetchMaterial from './UseFetchMaterial';
 
-type MaterialProps={
-  materials:MaterialType[]
+type MaterialProps = {
+  materials: MaterialType[]
 }
 
 const Material = () => {
-  const navigate =  useNavigate()
-  const {data:materials, isPending} = useFetchMaterialList('http://localhost:8000/materials');
+  const navigate = useNavigate()
+  const { data: materials, isPending } = useFetchMaterialList('http://localhost:8000/materials');
   return (
     <>
-        <div>
-            所有詳細資料、照片、buy and scrap button
+      <div>
+        所有詳細資料、照片、buy and scrap button
 
-        </div>
-        {/* {materials && <MaterialList materials={materials}/>} */}
-        
-        <button onClick={() => navigate(-1)}>go back</button>
+      </div>
+      {/* {materials && <MaterialList materials={materials}/>} */}
+
+      <button onClick={() => navigate(-1)}>go back</button>
     </>
   )
 }
--- frontend/src/pages/MaterialAndToolPage.tsx
+++ frontend/src/pages/MaterialAndToolPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link} from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import { MaterialType } from '../component/MaterialAndTool/MaterialType';
 import { MaterialList } from '../component/MaterialAndTool/MaterialList';
 import useFetchMaterialList from '../component/MaterialAndTool/UseFetchMaterialList'
@@ -6,18 +6,22 @@ import useFetchMaterial from '../component/MaterialAndTool/UseFetchMaterial';
 
 const MaterialAndToolPage = () => {
   const navigate = useNavigate()
-  const {data:materials, isPending} = useFetchMaterialList('http://localhost:8000/materials')
+  const { data: materials, isPending } = useFetchMaterialList('http://localhost:8000/materials')
   return (
     <>
       <div>Material & Tool Page</div>
       <p>應該要有搜尋欄、所有材料與工具，旁邊有樹狀結構的sidebar便於查詢(還需要顯示路徑在上方)</p>
       <p>每個材料的card包含photo,fullname,position,borrow_button</p>
-      <p><b>Material</b></p>
-      1.  <Link to={'/MaterialAndTool/Material/'+0}>Each Material</Link> {/* +id */}
-      <div>{materials && <MaterialList materials={materials}/>}</div>
-      <p><b>Tool</b></p>
-      1.  <Link to={'/MaterialAndTool/Tool/'+0}>Each Tool</Link> {/* +id */}
-      <br></br>
+      <p>
+        <b>Material</b>
+      </p>
+      <Link to={'/MaterialAndTool/Material/' + 0}>Each Material</Link> {/* +id */}
+      <div>{materials && <MaterialList materials={materials} />}</div>
+      <p>
+        <b>Tool</b>
+      </p>
+      <Link to={'/MaterialAndTool/Tool/' + 0}>Each Tool</Link> {/* +id */}
+
       <button onClick={() => navigate(-1)}>go back</button>
     </>
   )
