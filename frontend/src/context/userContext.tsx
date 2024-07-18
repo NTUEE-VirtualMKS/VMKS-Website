31a43e53a273ba7a3a36958cbd3f2625a15c3110
ChenBingWei1201 <rain4030633@gmail.com>
2024-07-18 12:58:42 +0800

    EDITOR-#187 [Frontend] AuthorizedCodePage UI and remove mui

--- frontend/src/context/userContext.tsx
+++ frontend/src/context/userContext.tsx
@@ -1,6 +1,6 @@
 // TODO: solve the incorrect user id problem
 import { createContext, useContext, useState, useCallback } from "react";
-import { useLazyQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { ALL_USER_QUERY } from "@/graphql";
 import type { UserType } from "@/shared/type.ts";
 import { useToast } from "@/components/ui/use-toast";
@@ -24,16 +24,29 @@ type UserProviderProps = {
 export const UserProvider = ({ children }: UserProviderProps) => {
   const [user, setUser] = useState<UserType | null>(null);
   const { toast } = useToast();
-  const [getUsers] = useLazyQuery(ALL_USER_QUERY);
+  // const [getUsers] = useLazyQuery(ALL_USER_QUERY);
+  const {
+    data,
+    loading: usersLoading,
+    error: usersError,
+  } = useQuery(ALL_USER_QUERY);
   const signIn = useCallback(async () => {
-    const { data } = await getUsers();
-
+    // const { data } = await getUsers();
+    if (usersLoading) {
+      toast({ title: "Loading..." });
+    }
+    if (usersError) {
+      toast({ title: `${usersError.message}`, variant: "destructive" });
+      throw new Error(usersError.message);
+    }
     if (!data?.AllUser) {
-      return console.error("No user found");
+      toast({ title: "No user found", variant: "destructive" });
+      throw new Error("No user found");
     }
     const [findUser] = data.AllUser.filter((e) => e?.id === 1); // incorrect
     if (!findUser) {
-      return console.error("No user found");
+      toast({ title: "User not found", variant: "destructive" });
+      throw new Error("User not found");
     } else {
       setUser({
         id: findUser.id,
@@ -50,7 +63,7 @@ export const UserProvider = ({ children }: UserProviderProps) => {
       });
       toast({ title: "Sign in successfully!" });
     }
-  }, [getUsers]);
+  }, [toast, usersError, usersLoading, data]);
 
   const logOut = () => {
     setUser(null);
